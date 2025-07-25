5d32cd4e036a3771e432ae18c75f75babe9961fe
Agustin Harfuch <agustin.harfuca@gmail.com>
2025-07-25 13:43:29 +0200

    Change configuration Change configuration on the prj.conf file in order to activate printk, define the time slice and define the pripority of the time slice.

--- hummidity-project/thread/src/main.c
+++ hummidity-project/thread/src/main.c
@@ -1,18 +1,30 @@
 /*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ * Copyright (c) Kernergetiks
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/kernel.h>
-#define SLEEP_TIME_MS 100U
+#include <zephyr/sys/printk.h>
 
-int main(void)
+#define STACKSIZE 1024
+
+/* STEP 5 - Change the priority of thread0 to 6 */
+#define THREAD0_PRIORITY 7
+#define THREAD1_PRIORITY 7
+
+void thread0(void)
 {
-	printf("Hello Houston, from Apollo 11! %s\n", CONFIG_BOARD_TARGET);
-    printk("Message in a bottle.\n");
-    while (1)
-    {
-        k_msleep(SLEEP_TIME_MS);
-    }
+	while (1) {
+            printk("Hello, I am thread0\n");
+            k_busy_wait(500000);
+	}
+}
+
+void thread1(void)
+{
+	while (1) {
+            printk("Hello, I am thread1\n");
+            k_busy_wait(1000000);
+	}
 }
